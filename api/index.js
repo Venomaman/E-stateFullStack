3dd870031e629e569be06773906e4d7bf263d156
Venomaman <amankumaranand111@gmail.com>
2024-06-01 20:37:59 +0530

    created a test api routes with proper folder structure chk fr ref.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL).then(()=>{
@@ -15,3 +16,5 @@ const app = express();
 app.listen(3000, ()=>{
     console.log('server ruuning on port 3000');
 })
+
+app.use('/api/user', userRouter)

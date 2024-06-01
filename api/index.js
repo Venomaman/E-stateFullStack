870e38b129996a639ade14108edf9224e6847cec
Venomaman <amankumaranand111@gmail.com>
2024-06-01 19:45:01 +0530

    mongodb database connection added.

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log('connected to mongoDB');
+})
+.catch((err)=>{
+    console.log(err);
+})
 
 const app = express();
 

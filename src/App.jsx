06a0b31e8fdccea7236fd6bbb6cc07f13458afe1
Venomaman <amankumaranand111@gmail.com>
2024-06-01 18:50:27 +0530

    header ui added with different pages link

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Signin from './pages/Signin'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignUp from './pages/SignUp'
+import Home from './pages/Home'
+import Header from './component/Header'
+
+export default function App() {
+  return <BrowserRouter>
+  <Header/> 
+  <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/signin' element={<Signin/>}/>
+    <Route path='/signup' element={<SignUp/>}/>
+    <Route path='/about' element={<About/>}/>
+    <Route path='/profile' element={<Profile/>}/>
+  </Routes>  
+  </BrowserRouter>
+}
+
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,7 @@
+import react from 'react'
+
+export default function Profile(){
+    return(<div>
+        Profile..
+    </div>);
+}

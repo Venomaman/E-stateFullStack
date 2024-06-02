e4b8601e116a11c53b4c6ee1ebd617ce3be0d2c3
Venomaman <amankumaranand111@gmail.com>
2024-06-02 13:38:37 +0530

    Sign up page Ui added in Client..

--- api/index.js
+++ api/index.js
@@ -1,35 +1,36 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';  
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL).then(()=>{
-    console.log('connected to mongoDB');
-})
-.catch((err)=>{
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("connected to mongoDB");
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
 const app = express();
 app.use(express.json());
 
-app.listen(3000, ()=>{
-    console.log('server ruuning on port 3000');
-})
+app.listen(3000, () => {
+  console.log("server ruuning on port 3000");
+});
 
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
-
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 //middleware..
-app.use((err, req, res, next)=>{
-    const statusCode = err.statusCode || 500;
-    const msg = err.msg || 'Internal server errorr';
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        msg
-    })
-})
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const msg = err.msg || "Internal server errorr";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    msg,
+  });
+});
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold py-12">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg outline-none"
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg outline-none"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg outline-none"
+          id="password"
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg hover:opacity-90 disabled:opacity-70">
+          Sign up
+        </button>
+      </form>
+      <div className="flex gap-2 justify-center pt-4">
+        <p>Have an account?</p>
+        <Link to={"/signin"}>
+          <span className="text-blue-700">Sign in</span>
+        </Link>
+      </div>
+    </div>
+  );
 }

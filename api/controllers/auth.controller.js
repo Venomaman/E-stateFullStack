d533b6fe1793f53c5783141f912c042fc082deba
Venomaman <amankumaranand111@gmail.com>
2024-06-03 11:11:46 +0530

    added the signin api logic to backend..

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,19 +1,48 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   // console.log(req.body);
   const { username, email, password } = req.body;
   if (!username || !email || !password) {
-    return res.status(400).json({ error: 'Username, email, and password are required.' });
+    return res
+      .status(400)
+      .json({ error: "Username, email, and password are required." });
   }
   const hashedPass = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ username, email, password:hashedPass});
+  const newUser = new User({ username, email, password: hashedPass });
   try {
-    await newUser.save()
-    res.status(201).json('User created successfully!')
+    await newUser.save();
+    res.status(201).json("User created successfully!");
   } catch (error) {
-     next(error);
+    next(error);
   }
+};
 
+//signin api logic....
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({
+      error: "email and password required!",
+    });
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "user not found!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "wrong creds!"));
+    //jwt token logic...
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY);
+    //we donot want user password to return....
+    const { password: pass, ...userInfoWithoutPass } = validUser._doc;
+    //store cookie in side browser..
+    res
+      .cookie("access_token", token, { httpOnly: true }) //can add epires time also like--> expires: new Date(Date.now() + 24 *60*60*1000)
+      .status(200)
+      .json(userInfoWithoutPass);
+  } catch (error) {}
 };
--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.use("/api/auth", authRouter);
 //middleware..
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal server errorr';
+  const message = err.message || "Internal server errorr";
   return res.status(statusCode).json({
     success: false,
     statusCode,

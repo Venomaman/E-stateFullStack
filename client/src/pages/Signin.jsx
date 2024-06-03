d8dd332e064005ca09a89ca218a9c351c1301967
Venomaman <amankumaranand111@gmail.com>
2024-06-03 17:48:44 +0530

    added redux toolkit ui/client..

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,7 +1,97 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice";
 
 export default function Signin() {
+  const [formData, setFormData] = useState({});
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  const {loading, error} = useSelector((state)=>state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  
+  //handle submit form click..
+  const handleSubmit = async (e) => {
+
+    //prevent reloading of webpage...
+    e.preventDefault();
+
+    //handeling no input field data...
+    if (!formData.email || !formData.password) {
+      dispatch(signInFailure("all fields are required"));
+      return;
+    }
+
+    try {
+     // setLoading(true);
+     dispatch(signInStart())
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+        // setLoading(false);
+        // setError(data.message);
+        return;
+      }
+      // setLoading(false);
+      // setError(null);
+      dispatch(signInSuccess(data))
+
+      navigate("/");
+    } catch (error) {
+      // setLoading(false);
+      // setError(error.message);
+      dispatch(signInFailure(error.message));
+    }
+  };
+
   return (
-    <div>Signin</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold py-12">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg outline-none"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg outline-none"
+          id="password"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg hover:opacity-90 disabled:opacity-70"
+        >
+          {loading ? "Loading..." : "Sign in"}
+        </button>
+      </form>
+      <div className="flex gap-2 justify-center pt-4">
+        <p>Dont Have an account?</p>
+        <Link to={"/signup"}>
+          <span className="text-blue-700">Sign up</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
+    </div>
+  );
 }

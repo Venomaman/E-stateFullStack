23f9abc8f4b8463d505569d812791e541dc903fd
Venomaman <amankumaranand111@gmail.com>
2024-06-06 19:10:12 +0530

    added Update Api logic to backend..

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,35 @@
-export const test=(req, res)=>{
-    res.json({
-        msg:'Api route is Working!',
-    })
-}
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+
+export const test = (req, res) => {
+  res.json({
+    msg: "Api route is Working!",
+  });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "you can update only your account!"));
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          //using set because user sometime want to update/change one data or multiple data so it will ignore other/rest data..
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    ); //it will set the new information/ else u will get the old information..
+
+    const { password, ...rest } = updateUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {}
+};
---
+++ api/utils/verifyUser.js
@@ -0,0 +1,14 @@
+import { errorHandler } from "./error.js";
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+
+  if (!token) return next(errorHandler(401, "Unauthorizedd..."));
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+    if (err) return next(errorHandler(403, "Forbidden.."));
+
+    req.user = user;
+    next();
+  });
+};

a9e7c1729fcfbc02800a4571f4339f6f84ec34f9
Venomaman <amankumaranand111@gmail.com>
2024-06-06 18:27:03 +0530

    added the image upload function..using the firebase storage..

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,20 +1,109 @@
-import react from 'react'
-import { useSelector } from 'react-redux'
+import react, { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import { app } from "../firebase";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 
-export default function Profile(){
-    const {currentUser} = useSelector(state=>state.user)
-    return<div className='p-3 max-w-lg mx-auto'>
-        <h1 className='text-3xl text-center font-semibold my-7'>Profile</h1>
-        <form className='flex flex-col gap-4'>
-            <img src={currentUser.avatar} alt='profile' className='rounded-full size-24 object-cover cursor-pointer self-center'/>
-            <input type='text' placeholder='username' id='username' className='border p-3 rounded-lg'/>
-            <input type='email' placeholder='email' id='email' className='border p-3 rounded-lg'/>
-            <input type='password' placeholder='password' id='password' className='border p-3 rounded-lg'/>
-            <button className='bg-slate-700 p-3 rounded-lg text-white hover:opacity-80'>Update</button>
-        </form>
-        <div className='flex justify-between mt-5'>
-            <span className='text-red-800 cursor-pointer' >Delete Account</span>
-            <span className='text-red-600 cursor-pointer'>Sign Out</span>
-        </div>
+export default function Profile() {
+  const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [filepercentage, setFilePercentage] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+  console.log(filepercentage)
+  console.log(formData);
+
+  //   change your firebase storage rules: ->
+
+  //   allow read;
+  //   allow write: if
+  //   request.resource.size < 2 * 1024 * 1024 &&
+  //   request.resource.contentType.matches('image/.*')
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on("state_changed",
+     (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      setFilePercentage(Math.round(progress));
+    },
+    (error) => {
+      setFileUploadError(true);
+    },
+    () => {
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        setFormData({ ...formData, avatar: downloadURL });
+      });
+    }
+   );
+  };
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+        
+        />
+        <img
+          onClick={() => fileRef.current.click()}
+          src={formData.avatar || currentUser.avatar}
+          alt="profile"
+          className="rounded-full size-24 object-cover cursor-pointer self-center"
+        />
+        <p className="text-sm self-center">
+            { fileUploadError ? (<span>Error while uploading image</span>)
+             : filepercentage > 0 && filepercentage< 100 ? (<span className="text-red-700">
+                {`Uploading ${filepercentage} %.... `}
+              </span>) : (filepercentage === 100) ? (<span className="text-gray-700">Successfully Uploaded!</span>) : ""
+            }
+        </p>
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          id="email"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="border p-3 rounded-lg"
+        />
+        <button className="bg-slate-700 p-3 rounded-lg text-white hover:opacity-80">
+          Update
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-800 cursor-pointer">Delete Account</span>
+        <span className="text-red-600 cursor-pointer">Sign Out</span>
+      </div>
     </div>
+  );
 }

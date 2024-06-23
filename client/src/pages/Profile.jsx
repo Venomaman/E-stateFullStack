8175a8368a8f667a29ebf8231642ebd656e041fc
Venomaman <amankumaranand111@gmail.com>
2024-06-23 13:31:02 +0530

    completed show users listing functionality..

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import react, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { app } from "../firebase";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import {
   getDownloadURL,
   getStorage,
@@ -30,6 +30,8 @@ export default function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccessMsg, setUpdateSuccessMsg] = useState(false);
+  const [showListingError, setShowListingError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   const dispatch = useDispatch();
   console.log(filepercentage);
   console.log(formData);
@@ -131,6 +133,21 @@ export default function Profile() {
     }
   };
 
+  const handleShowListing = async () => {
+    try {
+      setShowListingError(false);
+      const res = await fetch(`api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingError(true);
+        return;
+      }
+      setUserListings(data);
+    } catch (error) {
+      setShowListingError(true);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Profile</h1>
@@ -190,11 +207,14 @@ export default function Profile() {
           {loading ? "loading..." : "Update"}
         </button>
 
-      <Link className="bg-green-800 p-3 rounded-lg text-white text-center hover:opacity-80" to ={"/create-listing"}>
-        Create Listing
-      </Link>
+        <Link
+          className="bg-green-800 p-3 rounded-lg text-white text-center hover:opacity-80"
+          to={"/create-listing"}
+        >
+          Create Listing
+        </Link>
       </form>
-    
+
       <div className="flex justify-between mt-5">
         <span
           onClick={handleDeleteUser}
@@ -209,6 +229,52 @@ export default function Profile() {
       <p className="text-green-700">
         {updateSuccessMsg ? "user updated successfully !" : ""}
       </p>
+      <div className="pt-3">
+        <button
+          onClick={handleShowListing}
+          className="text-green-900 w-full border p-2 rounded-xl border-slate-500 hover:shadow-xl"
+        >
+          Show Listing
+        </button>
+        <p className="text-red-600">
+          {showListingError ? "Error showing listing" : ""}
+        </p>
+        {userListings && userListings.length > 0 && (
+          <div className="flex flex-col gap-4">
+            <h1 className="text-center my-7 text-3xl font-semibold">
+              Your Listings..
+            </h1>
+            {userListings.map((listing) => (
+              <div
+                key={listing._id}
+                className="border-2 bg-slate-50 rounded-xl p-3 flex justify-between items-center gap-10"
+              >
+                <Link to={`/listing/${listing._id}`}>
+                  <img
+                    src={listing.imageUrls[0]}
+                    alt="listing cover"
+                    className="size-20 object-contain"
+                  />
+                </Link>
+                <Link
+                  className="text-slate-700 font-semibold flex-1 hover:underline truncate"
+                  to={`/listing/${listing._id}`}
+                >
+                  <p>{listing.name}</p>
+                </Link>
+                <div className="items-center">
+                  <button className="text-red-600 uppercase border border-slate-500 p-2 rounded-xl hover:shadow-xl ">
+                    Delete
+                  </button>
+                  <button className="text-green-600 uppercase border border-slate-500 p-2 rounded-xl hover:shadow-xl ml-2">
+                    Edit
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
